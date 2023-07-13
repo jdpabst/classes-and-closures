1825da5af6823dd6c979bf512098b7912b48412f
Jessica Pabst <jess@Lorens-MacBook-Pro.local>
2023-07-13 16:46:36 -0600

    finished classes

--- classes.js
+++ classes.js
@@ -29,7 +29,18 @@
   Call your class Employee and receive all the data in the constructor in the order listed above.
 */
 
-//Code Here
+class Employee {
+  constructor(first_name, last_name, email, age){
+    this.first_name = first_name;
+    this.last_name= last_name;
+    this.email = email;
+    this.age = age;
+  }
+
+  makeWidget(){
+    return this.first_name + ' ' + this.last_name + ' Widget';
+  }
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -47,7 +58,27 @@
   Call your new class Manager
 */
 
-//Code Here
+class Manager {
+  constructor(first_name, last_name, email, age, reports){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+    this.reports = [];
+  }
+
+  makeWidget(){
+    return this.first_name + ' ' + this.last_name + ' Widget';
+  }
+  hire(employee){
+    this.reports.push(employee)
+    return this.reports
+  }
+  fire(index){
+    this.reports.splice(index, 1);
+    return this.reports
+  }
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -71,7 +102,45 @@
   Call your new class ProgressiveManager
 */
 
-//Code Here
+class ProgressiveManager {
+  constructor(first_name, last_name, email, age, reports, title, bonus){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+    this.reports = [];
+    this.title = "Not a manager";
+    this.bonus = 0;
+  }
+
+  makeWidget(){
+    return this.first_name + ' ' + this.last_name + ' Widget';
+  }
+  hire(employee) {
+    this.reports.push(employee);
+  
+    if (this.reports.length === 0) {
+      this.title = "Not a manager";
+    } else if (this.reports.length >= 1 && this.reports.length <= 3) {
+      this.title = 'Barely Manager';
+    } else if (this.reports.length >= 4 && this.reports.length <= 10) {
+      this.title = 'Mostly Manager';
+    } else if (this.reports.length >= 11 && this.reports.length <= 50) {
+      this.title = 'Manager';
+    } else if (this.reports.length >= 51 && this.reports.length <= 100) {
+      this.title = 'Manager Plus';
+    } else if (this.reports.length >= 101) {
+      this.title = 'Bestest Manager';
+    }
+  
+    return this.reports;
+  }
+  fire(index){
+    this.reports.splice(index, 1);
+    this.bonus += 100;
+    return this.reports
+  }
+}
 
 
 
@@ -97,7 +166,26 @@
         - This function returns an anonymous function that is called when the machine is done rebooting
         - The anonymous function should decrease wear_and_tear_count by 10, and set needs_reboot to false
 */
+class Machine {
+  constructor(){
+    this.widgets_made_count = 0;
+    this.wear_and_tear_count = 0;
+    this.needs_reboot = false;
+  }
 
-//Code Here
+  makeWidgets(num){
+    this.widgets_made_count += num;
+    this.wear_and_tear_count += Math.floor(num / 50)
+  }
+  fixMachine(){
+    this.needs_reboot = true;
+  }
+  reboot(){
+    return () => {
+      this.wear_and_tear_count -= 10;
+      this.needs_reboot = false
+    }
+  }
+}
 
 
